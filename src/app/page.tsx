cf532b3587ad9e8cf9a550063fbe79510d05638c
Noor <thenoorulhassan.dev@gmail.com>
2026-06-05 15:35:13 +0500

    Checking things out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from "@/components/sections/hero";
 import { Projects } from "@/components/sections/projects";
 import { About } from "@/components/sections/about";
 import { Skills } from "@/components/sections/skills";
-import { Experience } from "@/components/sections/experience";
+// import { Experience } from "@/components/sections/experience";
 import { Contact } from "@/components/sections/contact";
 
 export default function Home() {
@@ -16,7 +16,7 @@ export default function Home() {
         <Projects />
         <About />
         <Skills />
-        <Experience />
+        {/* <Experience /> */}
         <Contact />
       </main>
       <SiteFooter />
